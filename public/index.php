fa1f4c87eb174d73645f8f9e923acc0c2fda8d6a
HOARAU Mathieu <Mathieuhoarau@laposte.net>
2018-10-02 10:16:49 +0200

    mvc quete 2

--- public/index.php
+++ public/index.php
@@ -1,3 +1,12 @@
 <?php
-require __DIR__ . '/../src/Controller/ItemController.php';
+// chargement de l'autoload en début de fichier
+require __DIR__ . '/../vendor/autoload.php';
+//...
+
+$connexion = new \Controller\ItemController();
+$connexion->index();
+
+
+
 ?>
+
