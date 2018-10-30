7074741cd37c740707560caa94f048125d948e44
Gallier Guillaume <guillaume.gallier@sfr.fr>
2018-10-30 14:27:46 +0100

    ajout de la map fictive avec ico

--- app/routing.php
+++ app/routing.php
@@ -1,16 +1,8 @@
 <?php
 // routing.php
 $routes = [
-    'Item' => [ // Controller
-        ['index', '/', 'GET'], // action, url, method
-        ['show', '/item/{id:\d+}', 'GET'], // action, url, method
-        ['add', '/item/add', ['GET', 'POST']], // action, url, method
-        ['edit', '/item/edit/{id:\d+}', ['GET', 'POST']], // action, url, method
-        ['delete', '/item/delete/{id:\d+}', 'GET'],
-    ],
-    'Category' => [
-        ['indexCategory', '/category', 'GET'],
-        ['showCategory', '/category/{id}', 'GET'],
-    ]
+    'Map' => [ // Controller
+        ['map', '/', 'GET'], // action, url, method
 
+    ]
 ];
