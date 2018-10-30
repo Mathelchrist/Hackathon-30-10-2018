a512dcc170526017fe633fd4910ae7bb52c25d17
HOARAU Mathieu <Mathieuhoarau@laposte.net>
2018-10-30 14:27:27 +0100

    clean routing

--- app/routing.php
+++ app/routing.php
@@ -1,19 +1,7 @@
 <?php
 // routing.php
 $routes = [
-    'Item' => [ // Controller
-        ['index', '/', 'GET'], // action, url, method
-        ['show', '/item/{id:\d+}', 'GET'], // action, url, method
-        ['add', '/item/add', ['GET', 'POST']], // action, url, method
-        ['edit', '/item/edit/{id:\d+}', ['GET', 'POST']], // action, url, method
-        ['delete', '/item/delete/{id:\d+}', 'GET'],
-    ],
-    'Category' => [
-        ['indexCategory', '/category', 'GET'],
-        ['showCategory', '/category/{id}', 'GET'],
-    ],
     'Option' => [
         ['showOption', '/option', 'GET'],
     ]
-
 ];
