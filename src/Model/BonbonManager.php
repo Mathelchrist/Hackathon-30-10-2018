9470454443c5065cfccfa2a52d280a2bad777793
Patricia Batjom <patriciabatjom@gmail.com>
2018-10-30 21:09:05 +0100

    fin de la page

---
+++ src/Model/BonbonManager.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sylvain
+ * Date: 07/03/18
+ * Time: 18:20
+ * PHP version 7
+ */
+
+namespace Model;
+
+/**
+ *
+ */
+class ItemManager extends AbstractManager
+{
+    /**
+     *
+     */
+    const TABLE = 'bonbon';
+
+    /**
+     *  Initializes this class.
+     */
+    public function __construct(\PDO $pdo)
+    {
+        parent::__construct(self::TABLE, $pdo);
+    }
+
+
+}
