20bce28fa73060437f7e2b67c5e07f25734585f4
Patricia Batjom <patriciabatjom@gmail.com>
2018-10-30 17:53:56 +0100

    geolocalisation

--- src/Controller/BonbondexController.php
+++ src/Controller/BonbondexController.php
@@ -14,5 +14,18 @@ class BonbondexController extends AbstractController
         return $this->twig->render('/bonbondex.html.twig', ['items' => $items]);
     }
 
+    function getDistance($lat1, $lng1, $lat2, $lng2) {
+        $earth_radius = 6378137;   // Terre = sphère de 6378km de rayon
+        $rlo1 = deg2rad($lng1);
+        $rla1 = deg2rad($lat1);
+        $rlo2 = deg2rad($lng2);
+        $rla2 = deg2rad($lat2);
+        $dlo = ($rlo2 - $rlo1) / 2;
+        $dla = ($rla2 - $rla1) / 2;
+        $a = (sin($dla) * sin($dla)) + cos($rla1) * cos($rla2) * (sin($dlo) * sin($dlo));
+        $d = 2 * atan2(sqrt($a), sqrt(1 - $a));
+        return ($earth_radius * $d);
+    }
+
 }
 
