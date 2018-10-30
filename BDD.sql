3cb80edf5e78612d16f95baed60b97d743150245
milonte <milonte@gmaim.com>
2018-10-30 12:04:19 +0100

    creation BDD

---
+++ BDD.sql
@@ -0,0 +1,246 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb5
+-- https://www.phpmyadmin.net/
+--
+-- Client :  localhost:3306
+-- Généré le :  Mar 30 Octobre 2018 à 12:00
+-- Version du serveur :  5.7.24-0ubuntu0.18.04.1
+-- Version de PHP :  7.2.10-0ubuntu0.18.04.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `hackaton`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `adresse`
+--
+
+CREATE TABLE `adresse` (
+  `id` int(11) NOT NULL,
+  `longitude` float NOT NULL,
+  `latitude` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `adresse`
+--
+
+INSERT INTO `adresse` (`id`, `longitude`, `latitude`) VALUES
+(1, 47.919, 1.89869);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `bonbon`
+--
+
+CREATE TABLE `bonbon` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(100) NOT NULL,
+  `categorie_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `bonbon`
+--
+
+INSERT INTO `bonbon` (`id`, `nom`, `categorie_id`) VALUES
+(1, 'snickers', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `bonbondex`
+--
+
+CREATE TABLE `bonbondex` (
+  `id` int(11) NOT NULL,
+  `adresse_id` int(11) NOT NULL,
+  `bonbon_id` int(11) NOT NULL,
+  `quantité` int(11) DEFAULT NULL,
+  `joueur_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `bonbondex`
+--
+
+INSERT INTO `bonbondex` (`id`, `adresse_id`, `bonbon_id`, `quantité`, `joueur_id`) VALUES
+(2, 1, 1, 42, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `categorie`
+--
+
+CREATE TABLE `categorie` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `categorie`
+--
+
+INSERT INTO `categorie` (`id`, `nom`) VALUES
+(1, 'degueulasse');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `joueur`
+--
+
+CREATE TABLE `joueur` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(100) NOT NULL,
+  `prenom` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `joueur`
+--
+
+INSERT INTO `joueur` (`id`, `nom`, `prenom`) VALUES
+(1, 'chuuu', 'Pika');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `quantite`
+--
+
+CREATE TABLE `quantite` (
+  `id` int(11) NOT NULL,
+  `adresse_id` int(11) NOT NULL,
+  `bonbon_id` int(11) NOT NULL,
+  `quantite` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `quantite`
+--
+
+INSERT INTO `quantite` (`id`, `adresse_id`, `bonbon_id`, `quantite`) VALUES
+(1, 1, 1, 69);
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `adresse`
+--
+ALTER TABLE `adresse`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `bonbon`
+--
+ALTER TABLE `bonbon`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_bonbon_categorie` (`categorie_id`);
+
+--
+-- Index pour la table `bonbondex`
+--
+ALTER TABLE `bonbondex`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_adresse` (`adresse_id`),
+  ADD KEY `Fk_bonbon` (`bonbon_id`),
+  ADD KEY `FK_joueur` (`joueur_id`);
+
+--
+-- Index pour la table `categorie`
+--
+ALTER TABLE `categorie`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `joueur`
+--
+ALTER TABLE `joueur`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `quantite`
+--
+ALTER TABLE `quantite`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_quantite_adresse` (`adresse_id`),
+  ADD KEY `PK_quantite_bonbon` (`bonbon_id`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `adresse`
+--
+ALTER TABLE `adresse`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT pour la table `bonbon`
+--
+ALTER TABLE `bonbon`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT pour la table `bonbondex`
+--
+ALTER TABLE `bonbondex`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT pour la table `categorie`
+--
+ALTER TABLE `categorie`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT pour la table `joueur`
+--
+ALTER TABLE `joueur`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT pour la table `quantite`
+--
+ALTER TABLE `quantite`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `bonbon`
+--
+ALTER TABLE `bonbon`
+  ADD CONSTRAINT `FK_bonbon_categorie` FOREIGN KEY (`categorie_id`) REFERENCES `categorie` (`id`);
+
+--
+-- Contraintes pour la table `bonbondex`
+--
+ALTER TABLE `bonbondex`
+  ADD CONSTRAINT `FK_adresse` FOREIGN KEY (`adresse_id`) REFERENCES `adresse` (`id`),
+  ADD CONSTRAINT `FK_joueur` FOREIGN KEY (`joueur_id`) REFERENCES `joueur` (`id`),
+  ADD CONSTRAINT `Fk_bonbon` FOREIGN KEY (`bonbon_id`) REFERENCES `bonbon` (`id`);
+
+--
+-- Contraintes pour la table `quantite`
+--
+ALTER TABLE `quantite`
+  ADD CONSTRAINT `FK_quantite_adresse` FOREIGN KEY (`adresse_id`) REFERENCES `adresse` (`id`),
+  ADD CONSTRAINT `PK_quantite_bonbon` FOREIGN KEY (`bonbon_id`) REFERENCES `bonbon` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
