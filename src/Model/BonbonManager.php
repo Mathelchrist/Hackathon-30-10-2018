f0c872f587a03274af7b3be6ef8c17d87456c1ca
Amandine Turpin <amandyne.turpin@gmail.com>
2018-10-31 03:43:39 +0100

    Ajout SESSION ID sur requete

--- src/Model/BonbonManager.php
+++ src/Model/BonbonManager.php
@@ -29,8 +29,9 @@ class BonbonManager extends AbstractManager
 
     public function selectAllBonbon(): array
     {
+        session_start();
         $query = "SELECT b.id, x.quantite, x.longitude AS longitudebonbonpris, x.latitude AS latitudebonbonpris, b.nom, b.image_url, a.longitude, a.latitude FROM joueur j "
-            ." JOIN bonbondex x ON j.id = x.joueur_id AND j.id = 1"
+            ." JOIN bonbondex x ON j.id = x.joueur_id AND j.id = " . $_SESSION['id']
             ." RIGHT JOIN bonbon b ON x.bonbon_id = b.id"
             ." JOIN adresse a ON a.bonbon_id = b.id";
 
